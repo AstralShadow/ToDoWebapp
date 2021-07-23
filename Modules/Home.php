fee2b07e985001ff1579056ba6df79c0b3ea14a0
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2021-07-23 12:04:52 +0300

    Creating framework.

---
+++ Modules/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Modules;
+
+use Core\Module;
+
+/**
+ * Description of Home
+ *
+ * @author azcraft
+ */
+class Home implements Module
+{
+    //put your code here
+}
